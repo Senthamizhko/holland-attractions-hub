554bca545e3dd3a8986a92a0255a9136bf3df6ce
sarivannal <senthamizhko.arivannal@nielsen.com>
2024-11-25 13:22:30 +0100

    Adjust style +show search only in home page

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './style.scss';
 import { SearchContext } from '../../context/SearchContext';
@@ -11,16 +11,24 @@ const Header = () => {
     setSearchTerm(e.target.value.toLowerCase());
   };
 
+  useEffect(() => {
+    if (location.pathname !== '/' && searchTerm !== '') {
+      setSearchTerm('');
+    }
+  }, [location.pathname, searchTerm, setSearchTerm]);
+
   return (
     <header className="header">
       <Link className="header__link" to="/">Holland Attractions</Link>
-      {location.pathname === '/' && <input
-        type="text"
-        className="header__search"
-        placeholder="Search categories..."
-        value={searchTerm}
-        onChange={handleSearch}
-      />}
+      {location.pathname === '/' && (
+        <input
+          type="text"
+          className="header__search"
+          placeholder="Search categories..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      )}
       <Link to="/cart" className="header__nav__link">Cart</Link>
     </header>
   );
